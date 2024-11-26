c580f076bd823fcb5b0d83445fa63a8a2dc64b6a
CalebWiathaka <waithakacaleb6@gmail.com>
2024-11-26 23:46:17 +0300

    Filter project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['encrypted-tbn0.gstatic.com'], // Add domains here
+    },
+  };
+  
+  export default nextConfig;
+  
